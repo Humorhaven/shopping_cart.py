5b359d0e85999c34684298a2210954b65d26af91
Humorhaven <jjaaluk@gmail.com>
2024-01-13 22:16:39 -0700

    Initial commit

---
+++ program_code.py
@@ -0,0 +1,23 @@
+
+A = input('first shopping item: ')
+item1 = float(input ("enter price of item1 : "))
+B = input('second shopping item: ')
+item2 = float(input("enter price of item2 : "))
+C= input ('third shopping  item: ')
+item3= float(input("enter price of item3 : "))
+D= input ('4th shopping item: ')
+item4 = float(input("enter price of item4 : "))
+
+result = item1+item2+item3+item4
+print(result)
+if result < 100:
+    print("good job, your under budget")
+else:
+    print("Stop Buying")
+
+
+
+
+
+
+
---
+++ shopping_cart.py
@@ -0,0 +1,23 @@
+
+A=input('first item: ')
+B=input('second item: ')
+c=input('3rd item: ')
+d=input('4th item: ')
+def printtitle():
+    print('shoppinglist')
+    print('-------------')
+
+def printshoppinglist():
+    printtitle()
+    print(A)
+    print(B)
+    print(c)
+    print(d)
+
+
+print('start')
+printshoppinglist()
+print("-------------------")
+print('end')
+
+
